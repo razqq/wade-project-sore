3ee81936b214d0f4a21855748d875346b9457d3b
Yrazqq <ycroitorurazvan1@gmail.com>
2023-11-06 13:53:00 +0200

    Added  base react frontend component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="center">
+            Work in progress
+        </div>
+    );
+}
+
+export default App;
